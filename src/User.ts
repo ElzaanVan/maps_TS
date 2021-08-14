15cebb0c4ac7b5de8634cb86ae84b5d462c2721b
ElzaanVan <elzaanvanl@gmail.com>
2021-08-14 14:09:55 +0200

    Second commit
    This commit includes the index.html as well as the src director

---
+++ src/User.ts
@@ -0,0 +1,17 @@
+import faker from "faker";
+// install a type definition file if module does not have it 
+// Definitely Typed
+// Install npm module @types/{library name}
+
+class User {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    }
+
+    constructor() {
+
+    }
+};
+
